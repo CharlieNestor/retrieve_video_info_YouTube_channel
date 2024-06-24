be096296f6a88ad1e2e50dd88f8d34c18ab3331e
Carlo Baroni <138825949+CarloNestor@users.noreply.github.com>
2024-06-24 10:07:46 +0200

    Update infoYT_streamlit.py
    Adjust few things and add piece to retrieve historical data if either you don't have any or you have just a little.

--- infoYT_streamlit.py
+++ infoYT_streamlit.py
@@ -7,7 +7,7 @@ from get_infoYT import InfoYT
 # Function to get existing channel usernames from files
 def get_existing_channels() -> list[str]:
     """
-    Get the list of existing channel usernames from the files in the 'Channel_Videos' folder.
+    get the list of existing channel usernames from the files in the 'Channel_Videos' folder.
     """
     folder_path = 'Channel_Videos'
     if not os.path.exists(folder_path):
@@ -19,7 +19,7 @@ def get_existing_channels() -> list[str]:
 
 def get_video_url(channel_username: str) -> str:
     """
-    Get the URL of the first video of a channel.
+    get the URL of the first video of a channel.
     """
     filename = channel_username+'_videos.json'
     folder_path = 'Channel_Videos'
@@ -29,6 +29,7 @@ def get_video_url(channel_username: str) -> str:
     video_ids = list(videos_dict.keys())
     video_url = f"https://www.youtube.com/watch?v={video_ids[0]}"
     return video_url
+
 
 def main() -> None:
     """
@@ -44,6 +45,7 @@ def main() -> None:
     selected_channel = st.selectbox("Or select an existing channel", [""] + existing_channels)  # add first blank option
 
     if new_channel_url or selected_channel:
+        # Avoid selecting both options (remember that options stay selected in the different sessions)
         if new_channel_url and selected_channel:
             st.warning("Please select only one option.")
             return
@@ -56,13 +58,12 @@ def main() -> None:
                 info_yt = InfoYT(channel_url)
 
             st.write('##')
+            # Display channel information
             st.write('**CHANNEL INFORMATION:**')
             st.write(f"Channel name: **{info_yt.channel_username}**")
             st.write(f"Total published videos: {info_yt.num_videos}")
             if info_yt.check_history():
                 st.write(f'The number of videos already stored is: {len(info_yt.all_videos)}')
-                #st.write(f'The latest video stored was published: {info_yt.most_recent_date}')
-                #st.write(f'The oldest video stored was published: {info_yt.oldest_date}')
             else:
                 st.write("No videos stored for this channel.")
 
@@ -78,18 +79,43 @@ def main() -> None:
                     st.write("No videos stored for this channel.")
 
             st.write('###')
+            # Add a streamlit button to update the videos
             st.write('Retrieve and update the dataset with the latest videos:')
             if st.button("Update video "):
                 if info_yt.check_history():
-                    st.write(info_yt.update_videos(verbose=True))
+                    output = (info_yt.update_videos(streamlit=True))
+                    if len(output) > 0:
+                        st.write(f"I've found {len(output)} new videos to be added!")
+                        for title in output:
+                            st.warning(f"New video found: {title}")
                     info_yt.save_to_json()
                     st.success("Videos updated and saved!")
                 else:
                     st.warning("No videos stored for this channel. Impossible to update.")  
+
+            st.write('###')
+            # Add a streamlit button to download the historical data
+            st.write('Download historical data for the channel:')
+            if st.button("Download historical data"):
+                if not info_yt.check_history():
+                    output = info_yt.get_all_videos(max_videos=200, streamlit=True)
+                    if len(output) > 0:
+                        st.write(f"This download has retrieved {len(output)} videos.")
+                    info_yt.save_to_json()
+                    st.success("Historic data downloaded and saved!")
+                elif len(info_yt.all_videos) < 0.9*info_yt.num_videos:
+                    output = info_yt.get_all_videos(max_videos=100, streamlit=True)
+                    if len(output) > 0:
+                        st.write(f"I've found {len(output)} new videos to be added!")
+                    info_yt.save_to_json()
+                    st.success("Videos updated and saved!")
+                else:
+                    st.warning("Historic data already downloaded.") 
 
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
+
     main()
